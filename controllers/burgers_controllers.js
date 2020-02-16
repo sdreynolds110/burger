25078e8c99a2dab7968e3df47b87707803223945
Stephen Reynolds <stephenreynolds@Stephens-MBP.fios-router.home>
2020-02-15 20:50:13 -0500

    added js and handlebars

---
+++ controllers/burgers_controllers.js
@@ -0,0 +1,29 @@
+var express = require("express");
+var burgers = require("../models/burgers.js");
+
+var router = express.Router();
+router.get("/", function(req,res) {
+    burger.selectAll(function(data) {
+        res.render("index", {burgers: data})
+    });
+});
+
+router.post("/api/burgers" , function(req,res) {
+    const value = req.body.burger_name;
+    burgers.insertOne(value, function(result) {
+        res.json({id: result.insertId});
+    });
+});
+
+router.put("/api/burgers/:id", function(req,res) {
+    const condition = req.params.id;
+    burgers.updateOne(condition, function(result) {
+        if (result,affectedRow === 0) {
+              return res.status(404). end();
+            } else {
+              res.status(200).end();;
+            }
+    });
+});
+
+module.exports = router;
---
+++ models/burger.js
@@ -0,0 +1,21 @@
+var orm = require("../config/orm");
+
+const burger = {
+    selectAll: function(cb) {
+      orm.selectAll("burgers", function(res) {
+        cb(res);
+      });
+    },
+    insertOne: function(value, cb) {
+      orm.insertOne("burgers", "burger_name", value, function(res) {
+        cb(res);
+      });
+    },
+    updateOne: function(id, cb) {
+      orm.updateOne("burgers", "devoured", 1, "id", id, function(res) {
+        cb(res);
+      });
+    }
+  };
+    
+module.exports = burger;
